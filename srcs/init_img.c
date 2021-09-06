fda0c938d7dbb714690ac7411b4f97cda64bc12d
selee <selee@student.42lyon.fr>
2021-09-06 04:43:15 +0200

    cleaned up xpm files

--- srcs/init_img.c
+++ srcs/init_img.c
@@ -3,10 +3,10 @@
 void	init_tile_img(t_var_set *var)
 {
 	var->tile.wall.ptr = mlx_xpm_file_to_image(var->mlx,
-		"./assets_xpm/test_yellow32.xpm", &(var->tile.wall.width),
+		"./assets_xpm/sakura32.xpm", &(var->tile.wall.width),
 		&(var->tile.wall.height));
 	var->tile.space.ptr = mlx_xpm_file_to_image(var->mlx,
-		"./assets_xpm/test_green32.xpm", &(var->tile.space.width),
+		"./assets_xpm/grass32.xpm", &(var->tile.space.width),
 		&(var->tile.space.height));
 	var->tile.exit.ptr = mlx_xpm_file_to_image(var->mlx,
 		"./assets_xpm/house32.xpm", &(var->tile.exit.width),
@@ -16,7 +16,7 @@ void	init_tile_img(t_var_set *var)
 void	init_player_img(t_var_set *var)
 {
 	var->player.image.ptr = mlx_xpm_file_to_image(var->mlx,
-		"./assets_xpm/short_hair_girl_32.xpm", &(var->player.image.width),
+		"./assets_xpm/short_hair_girl32.xpm", &(var->player.image.width),
 		&(var->player.image.height));
 }
 
--- srcs/map_render.c
+++ srcs/map_render.c
@@ -1,9 +1,5 @@
 #include "../include/so_long.h"
 
-/* reduce lines:
- - put player and collec in a separate function
-*/
-
 void	map_put_to_win(t_var_set *var)
 {
 	int		i;
@@ -14,14 +10,12 @@ void	map_put_to_win(t_var_set *var)
 	{
 		j = 0;
 		while (j < var->map.column_count)
-		{
+		{		
+			mlx_put_image_to_window(var->mlx, var->win,
+					var->tile.space.ptr, j * TILE_SIZE, i * TILE_SIZE);
 			if (var->map.array[i][j] == '1')
 				mlx_put_image_to_window(var->mlx, var->win,
 					var->tile.wall.ptr, j * TILE_SIZE, i * TILE_SIZE);
-			else if (var->map.array[i][j] == '0' || var->map.array[i][j] == 'P'
-					|| var->map.array[i][j] == 'C' || var->map.array[i][j] == 'E')
-				mlx_put_image_to_window(var->mlx, var->win,
-					var->tile.space.ptr, j * TILE_SIZE, i * TILE_SIZE);
 			if (var->map.array[i][j] == 'E')
 				mlx_put_image_to_window(var->mlx, var->win,
 					var->tile.exit.ptr, j * TILE_SIZE, i * TILE_SIZE);
@@ -33,4 +27,3 @@ void	map_put_to_win(t_var_set *var)
 		i++;
 	}
 }
-
